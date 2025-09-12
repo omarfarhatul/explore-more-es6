1820e8ed8ff2fc01139383ed02a2925c157abbed
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-12 21:34:10 +0600

    Update

--- chain.js
+++ chain.js
@@ -1,3 +1,14 @@
 // how to use data access
 const data=[{id: 234, name: 'hossain', address: 'savar'}];
 console.log(data[0].address);
+
+const products = {
+    count: 5000,
+    data: [
+        {id: 1, name: 'lenovo laptop', price: 65000},
+        {id: 2, name: 'mac laptop', price: 165000}
+    ]
+}
+// second product price
+console.log(products.data[1].price);
+console.log(products.data[0].name);
