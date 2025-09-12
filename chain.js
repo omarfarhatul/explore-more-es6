340e3cc8fcf08cab8db3aed41bd2ddbf28c7c549
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-12 21:04:40 +0600

    first commit

---
+++ chain.js
@@ -0,0 +1,3 @@
+// how to use data access
+const data=[{id: 234, name: 'hossain', address: 'savar'}];
+console.log(data[0].address);
---
+++ recap.js
@@ -0,0 +1,27 @@
+/* 
+1. var let const
+2. default patameter
+3. templete string
+4. arrow function
+5. destructuring and spread operator
+6. Object.keys, Object.values, Object.entries
+7. for of used in array and string
+8. for in loop used in object
+*/
+
+const a=34;
+const numbers=[35, 23, 32];
+const person = {
+    name: 'Sakib Khan'
+}
+const message=`Hi, ${person.name} has a: ${a} access to ${numbers[2]}`
+
+if(true){
+
+}
+
+const square = x => x*x;
+const sum = (a, b) => a+b;
+
+const {}={x: 2, y: 5, z: 6, name: 'ovijeet', age: 55}
+const [first, second, last] =['sam', 'jam', 'kim']
