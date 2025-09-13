cd1ae0ef4e6c174f3cb7bf6e71eaa2e7f835d720
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-13 18:32:15 +0600

    added something

--- dot-breaket.js
+++ dot-breaket.js
@@ -1,8 +1,10 @@
 const person={
     name: 'Hossain',
-    job: 'Student'
+    job: 'Student',
+    3: 'Third'
 }
 const prop='job';
 console.log(person.job);
 console.log(person['job']);
 console.log(person[prop]);
+console.log(person['3']);
