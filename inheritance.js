411414ea75b418e480f8ca2725ba16bc2f387618
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-13 18:17:13 +0600

    Inheritance js

---
+++ inheritance.js
@@ -0,0 +1,27 @@
+class vehicle{
+    constructor(name, price){
+        this.name=name;
+        this.price=price;
+    }
+    moveBy(){
+    console.log('I trave by bus for all time Dhaka to Feni.');
+    }
+}
+const car=new vehicle('Toyota', 120000);
+console.log(car);
+
+
+class Bus extends vehicle{
+    constructor(name, price, seat, ticketPrice){
+        super(name, price);
+        this.seat=seat;
+        this.ticketPrice=ticketPrice;
+    }
+}
+
+class truck extends vehicle{
+    constructor(name, price, load){
+        super(name, price);
+        this.load=load;
+    }
+}
