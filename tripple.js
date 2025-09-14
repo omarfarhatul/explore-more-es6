043a77ea61bf798cca66cc057ba79880def15ed6
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-14 22:05:53 +0600

    Tripple

--- tripple.js
+++ tripple.js
@@ -1,3 +1,6 @@
+// always use ===
+// equal conparison doesn't work for non-primitive 
+
 const n1=4;
 const n2='4';
 if(n1===n2){
@@ -8,3 +11,11 @@ else{
 }
 
 // type conversion/casting
+const first=[3, 5, 7];
+const second=[3, 5, 7];
+if(first===second){
+    console.log('values are equal');
+}
+else{
+    console.log('values are not equal');
+}
